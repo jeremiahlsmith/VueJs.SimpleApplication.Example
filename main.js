56173fc1e915bf5935fc82573b5c6a26902fc91d
Jeremiah Smith <jeremiahlsmith@outlook.com>
2018-03-19 10:53:31 -0400

    further development in guide

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li><input type="checkbox" /> {{ todo.text }}</li>'
+})
+
+
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -10,21 +17,28 @@ var app4 = new Vue({
   el: '#app-4',
   data: {
     todos: [{
+        id:1,
         text: 'Learn Javascript'
       },
       {
+        id:2,
         text: 'Learn Vue'
       },
       {
+        id:3,
         text: 'Build something awesome'
       }
     ]
   },
   methods: {
-    alertBox: alertMessage()
+    alertBox: function() {alertMessage()}
+  },
+  created: function() {
+    console.log("First todo is " + this.todos[0].text)
   }
 })
 
 var alertMessage = function () {
   alert('Submit Successful!');
 }
+
